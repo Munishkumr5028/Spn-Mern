45babb91549b56ff81c16f23fdf3e662db072395
Munishkumr5028 <160215415+Munishkumr5028@users.noreply.github.com>
2025-06-23 13:17:58 +0530

    gallery css fix

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import './Gallery.css';
+import React, { useState } from "react";
+import "./Gallery.css";
 
-const galleryImages = Array.from({ length: 60 }, (_, i) => `https://picsum.photos/300/200?random=${i + 1}`);
+const galleryImages = Array.from(
+  { length: 60 },
+  (_, i) => `https://picsum.photos/300/200?random=${i + 1}`
+);
 
 function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="gallery-marquee">
       <div className="gallery-grid">
-        {galleryImages.concat(galleryImages).map((src, index) => (
-          <div key={index} className="gallery-item">
+        {galleryImages.map((src, index) => (
+          <div
+            key={index}
+            className="gallery-item"
+            onClick={() => setSelectedImage(src)}
+          >
             <img src={src} alt={`Gallery ${index + 1}`} />
             <span className="hover-border"></span>
           </div>
         ))}
       </div>
+
+      {selectedImage && (
+        <div className="lightbox" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="Enlarged view" />
+        </div>
+      )}
     </div>
   );
 }
